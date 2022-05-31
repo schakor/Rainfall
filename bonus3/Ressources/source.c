fce3b1b056785d86e2dc2bc152cf0fefc911f1e7
Simon CHAKOR <schakor@e3r7p2.clusters.42paris.fr>
2022-05-31 17:42:59 +0200

    Rainfall finally over

---
+++ bonus3/Ressources/source.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    char buffer1[66];
+    char buffer2[65];
+    FILE *fd;
+
+     fd = fopen("/home/user/end/.pass", "r");
+    if (fd == NULL || argc != 2) {
+        return 1;
+    }
+    fread(buffer1, 1, 66, fd);
+    buffer1[atoi(argv[1])] = 0;
+    fread(buffer2, 1, 65, fd);
+    fclose(fd);
+    if (strcmp(buffer1, argv[1]) == 0) {
+        execl("/bin/sh", "sh", NULL);
+    } else {
+        puts("");
+    }
+    return 0;
+}
